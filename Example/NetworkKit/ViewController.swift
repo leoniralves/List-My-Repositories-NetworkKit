11b54fd8c4fee87ce9714cafefd2be09a6fc3a3c
Leonir Deolindo <leonir.ad@gmail.com>
2019-04-07 16:55:58 -0300

    Initial commit

---
+++ Example/NetworkKit/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  NetworkKit
+//
+//  Created by Leonir Deolindo on 04/07/2019.
+//  Copyright (c) 2019 Leonir Deolindo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
